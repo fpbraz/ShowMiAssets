c347e4d45707a8bfb43242a8d795c7bd7de205ad
Felipe P Braz <fpbrazz@gmail.com>
2016-07-11 14:59:11 -0700

    First commit

---
+++ ITSManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ITSManagement
+//
+//  Created by Felipe P Braz on 2016-07-11.
+//  Copyright © 2016 ITSManagement. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
